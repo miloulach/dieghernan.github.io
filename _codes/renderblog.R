4e7d53fb054e1e63dfbe20e6c7aaa1bbc271f410
dieghernan <diego.hernangomezherrero@gmail.com>
2019-05-02 08:41:47 +0200

    Better chart

--- _codes/renderblog.R
+++ _codes/renderblog.R
@@ -9,7 +9,7 @@ diegpost <- function(dirname) {
   getwd()
   ezknit(
     "../2019-04-27-Using-CountryCodes.Rmd",
-    paste("_codes/", dirname,sep=""),
+    wd=paste("./_codes/", dirname,sep=""),
     out_dir = "../../_posts",
     fig_dir = paste("../_codes/", dirname,sep=""),
     keep_html = FALSE
