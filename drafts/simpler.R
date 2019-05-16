5ae56a6b9b4107946d49f2a67d0ab3382616e98f
dieghernan <diego.hernangomezherrero@gmail.com>
2019-05-16 17:30:32 +0200

    Create simpler.R

---
+++ drafts/simpler.R
@@ -0,0 +1,108 @@
+#1. Set up----
+rm(list = ls(all = TRUE))
+
+library(pacman)
+p_load(
+  sf,
+  raster,
+  dplyr,
+  cartography,
+  rnaturalearth,
+  openxlsx)
+
+testmap=getData("GADM",country="ESP",level=1)
+testmap_sf=st_as_sf(testmap)
+plot(st_geometry(testmap_sf))
+test2=testmap_sf[1:3,]
+plot(st_geometry(test2))
+
+
+hexsquaremap <- function(sf,
+                         tipo = "square",
+                         ancho = 50,
+                         grupo = F) {
+  if (tipo == "square") {
+    type = T
+  }
+  else{
+    type = F
+  }
+  initial = sf
+  initial$index_target = 1:nrow(initial)
+  target = st_geometry(initial)
+  grid = st_make_grid(
+    target,
+    ancho,
+    crs = st_crs(initial),
+    what = "polygons",
+    square = type
+  )
+  grid = st_sf(index = 1:length(lengths(grid)), grid)
+  cent_grid = st_centroid(grid)
+  cent_merge = st_join(cent_grid, initial["index_target"], left = F)
+  grid_new = inner_join(grid, st_drop_geometry(cent_merge))
+  if (grupo == F) {
+    noagrup = aggregate(
+      grid_new,
+      by = list(grid_new$index_target),
+      FUN = min,
+      do_union = FALSE
+    )
+    a = noagrup
+  }
+  else {
+    agrup = aggregate(
+      st_buffer(grid_new, dist = 0.5),
+      by = list(grid_new$index_target),
+      FUN = min
+    )
+    a = agrup
+  }
+  
+  data = st_drop_geometry(initial)
+  a = left_join(a, data)
+  a = a[names(sf)]
+  a = st_cast(a, "MULTIPOLYGON")
+  return(a)
+}
+
+dotsmap <- function(sf,
+                    ancho = 50) {
+  initial = sf
+  initial$index_target = 1:nrow(initial)
+  target = st_geometry(initial)
+  grid = st_make_grid(target,
+                      ancho,
+                      crs = st_crs(initial),
+                      what = "centers")
+  grid = st_sf(index = 1:length(lengths(grid)), grid)
+  cent_merge = st_join(grid, initial["index_target"], left = F)
+  grid_new = st_buffer(cent_merge, ancho / 2)
+  
+  a = aggregate(
+    grid_new,
+    by = list(grid_new$index_target),
+    FUN = min,
+    do_union = FALSE
+  )
+  data = st_drop_geometry(initial)
+  a = left_join(a, data)
+  a = a[names(sf)]
+  a = st_cast(a, "MULTIPOLYGON")
+  return(a)
+}
+
+hexno=hexsquaremap(testmap_sf,"hex",1,F)
+hexsi=hexsquaremap(testmap_sf,"hex",1,T)
+squno=hexsquaremap(testmap_sf,"square",1,F)
+squsi=hexsquaremap(testmap_sf,"square",1,T)
+dots=dotsmap(testmap_sf,1)
+dev.off()
+par(mfrow = c(3, 2), mar = c(1, 1, 1, 1))
+plot(st_geometry(hexno),col=colors()[60:90])
+plot(st_geometry(hexsi),col=colors()[60:90])
+plot(st_geometry(squno),col=colors()[60:90])
+plot(st_geometry(squsi),col=colors()[60:90])
+plot(st_geometry(dots),col=colors()[60:90])
+convex=st_convex_hull(testmap_sf)
+plot(st_geometry(convex),col=colors()[60:90])
