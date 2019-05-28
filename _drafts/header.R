afd90c84ea0b8df5dc4c0662ba7a0064697a1cb4
dieghernan <diego.hernangomezherrero@gmail.com>
2019-05-28 20:15:52 +0200

    Fix img

--- _drafts/header.R
+++ _drafts/header.R
@@ -2,13 +2,14 @@
 
 library(sf)
 library(rnaturalearth)
+library(dplyr)
 
 mapworld=st_read("~/R/mapslib/EUROSTAT/CNTR_RG_20M_2016_3857.shp") %>%
   st_transform(crs="+proj=robin")
 
 
 getwd()
-png("img/headerblog.png", width = 1000,height =544,
+png("/assets/img/headerblog.png", width = 800,
     units = "px",res=90)
 par(mar=c(0,0,0,0))
 plot(st_geometry(mapworld),
