d097056c09636ce3fab5ec42db4aa454d936f2ea
dieghernan <diego.hernangomezherrero@gmail.com>
2019-06-10 10:13:09 +0000

    Flags

--- wip/Mapflag.R
+++ wip/Mapflag.R
@@ -46,48 +46,126 @@ plot(st_geometry(SPAINV2),col="#FEFEE9",add=T)
 
 
 # 2. Flags----
-flags_wiki <- function(url,name){
+flags_wiki <- function(url, name) {
   require(curl)
-  curl_download(url,paste("assets/flags/Flag_",name,".svg.png",sep = ""))
-  c=brick(paste("assets/flags/Flag_",name,".svg.png",sep = ""))
+  curl_download(url, paste("assets/flags/Flag_", name, ".svg.png", sep = ""))
+  c = brick(paste("assets/flags/Flag_", name, ".svg.png", sep = ""))
   plotRGB(c)
 }
 
 flags_wiki(
   "https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Flag_of_Andaluc%C3%ADa.svg/800px-Flag_of_Andaluc%C3%ADa.svg.png",
-  "ES.AN")
+  "ES.AN"
+)
 flags_wiki(
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Flag_of_Aragon.svg/800px-Flag_of_Aragon.svg.png","ES.AR")
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Flag_of_Aragon.svg/800px-Flag_of_Aragon.svg.png",
+  "ES.AR"
+)
 flags_wiki(
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/Flag_of_Cantabria.svg/800px-Flag_of_Cantabria.svg.png","ES.CB")
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/Flag_of_Cantabria.svg/800px-Flag_of_Cantabria.svg.png",
+  "ES.CB"
+)
 flags_wiki(
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Flag_of_Castile-La_Mancha.svg/800px-Flag_of_Castile-La_Mancha.svg.png","ES.CM")
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Flag_of_Castile-La_Mancha.svg/800px-Flag_of_Castile-La_Mancha.svg.png",
+  "ES.CM"
+)
 flags_wiki(
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/1/13/Flag_of_Castile_and_Le%C3%B3n.svg/780px-Flag_of_Castile_and_Le%C3%B3n.svg.png","ES.CL")
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/1/13/Flag_of_Castile_and_Le%C3%B3n.svg/780px-Flag_of_Castile_and_Le%C3%B3n.svg.png",
+  "ES.CL"
+)
+# flags_wiki(
+#   "https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Flag_of_Catalonia.svg/640px-Flag_of_Catalonia.svg.png",
+#   "ES.CT"
+# )
+# Select only Ceuta since the shape is combined. Could be splitted but I just did this for clarity
+flags_wiki(
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/Flag_Ceuta.svg/800px-Flag_Ceuta.svg.png",
+  "ES.ML"
+)
+# flags_wiki(
+#   "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Flag_of_the_Community_of_Madrid.svg/800px-Flag_of_the_Community_of_Madrid.svg.png",
+#   "ES.MD"
+# )
+flags_wiki(
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/Bandera_de_Navarra.svg/750px-Bandera_de_Navarra.svg.png",
+  "ES.NA"
+)
+flags_wiki(
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Flag_of_the_Land_of_Valencia_%28official%29.svg/800px-Flag_of_the_Land_of_Valencia_%28official%29.svg.png",
+  "ES.VC"
+)
+flags_wiki(
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Flag_of_Extremadura_%28with_coat_of_arms%29.svg/800px-Flag_of_Extremadura_%28with_coat_of_arms%29.svg.png",
+  "ES.EX"
+)
+flags_wiki(
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Flag_of_Galicia.svg/800px-Flag_of_Galicia.svg.png",
+  "ES.GA"
+)
+flags_wiki(
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7b/Flag_of_the_Balearic_Islands.svg/800px-Flag_of_the_Balearic_Islands.svg.png",
+  "ES.PM"
+)
+flags_wiki(
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/Flag_of_La_Rioja_%28with_coat_of_arms%29.svg/800px-Flag_of_La_Rioja_%28with_coat_of_arms%29.svg.png",
+  "ES.LO"
+)
 
 
-
-CCAA="ES.CL"
+CCAA="ES.CT"
 shp=SPAINV2 %>% subset(HASC_1==CCAA)
 flag=brick(paste("assets/flags/Flag_",CCAA,".svg.png",sep = ""))
+plotRGB(flag)
 ratioflag=extent(flag)@xmax/extent(flag)@ymax
-neww=(extent(shp)@ymax-extent(shp)@ymin)*ratioflag+extent(shp)@xmin
+neww=1.3*(extent(shp)@ymax-extent(shp)@ymin)*ratioflag+extent(shp)@xmin
 new=c(extent(shp)@xmin,neww,extent(shp)@ymin,extent(shp)@ymax)
 extent(flag)<-new
 projection(flag)<- CRS(st_crs(shp)[["proj4string"]])
 f1=mask(flag,shp)
 
 
+
 CCAA="ES.CB"
 shp=SPAINV2 %>% subset(HASC_1==CCAA)
 flag=brick(paste("assets/flags/Flag_",CCAA,".svg.png",sep = ""))
 ratioflag=extent(flag)@xmax/extent(flag)@ymax
-neww=(extent(shp)@ymax-extent(shp)@ymin)*ratioflag+extent(shp)@xmin
+neww=1.3*(extent(shp)@ymax-extent(shp)@ymin)*ratioflag+extent(shp)@xmin
 new=c(extent(shp)@xmin,neww,extent(shp)@ymin,extent(shp)@ymax)
 extent(flag)<-new
 projection(flag)<- CRS(st_crs(shp)[["proj4string"]])
 f2=mask(flag,shp)
-plotRGB(f2,axes=T)
+
+f1
+f2_end=mask(f2,SPAINV2)
+
+plot(st_geometry(SPAINV2),border=NA)
+plotRGB(f1,add=T)
+plotRGB(f2,add=T)
+plot(st_geometry(SPAINV2),add=T)
+c=crop(f2_end,SPAINV2)
+
+res(f2_c)<-res(f1_c)
+origin(f2_c)<-origin(f1_c)
+
+c2=merge(f1_c,f2_c)
+
+c3=merge(f1,f2)
+
+c=raster(f1)
+c2=raster(f2)
+c3=merge(c,c2)
+
+
+
+c2=merge(f1,f2)
+c2=stack(f1,f2)
+c3=bind(f1,f2)
+plotRGB(c2)
+origin(f2)<-origin(f1)
+origin(f1)
+origin(f2)
+c3=brick(f1,f2)
+
 c2=brick(extent(SPAINV2))
 res(c2) <- res(f1)
 origin(c2)<-origin(f1)
